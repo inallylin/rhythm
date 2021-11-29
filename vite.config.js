26dc723c067b4210210bbb3af6f044f18148a240
Ally Lin <llin@ilink.com.tw>
2021-11-29 18:04:05 +0800

    add env

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import sass from 'rollup-plugin-sass'
 import coffeescript from 'rollup-plugin-coffee-script'
@@ -8,20 +9,24 @@ import nodeResolve from 'rollup-plugin-node-resolve'
  * https://vitejs.dev/config/
  * @type {import('vite').UserConfig}
  */
-export default {
-  plugins: [
-    vue(),
-    sass(),
-    coffeescript(),
-    nodeResolve({ extensions: ['.js', '.coffee'] }),
-    commonjs({
-      extensions: ['.js', '.coffee']
-    })
-  ],
-  server: {
-    host: 'localhost',
-    hmr: {
-      // port: 443,
+
+ export default defineConfig(({ mode }) => {
+   const env = loadEnv(mode, __dirname)
+   return {
+    plugins: [
+      vue(),
+      sass(),
+      coffeescript(),
+      nodeResolve({ extensions: ['.js', '.coffee'] }),
+      commonjs({
+        extensions: ['.js', '.coffee']
+      })
+    ],
+    server: {
+      host: 'localhost',
+      hmr: {
+        port: env.VITE_HMR_PORT,
+      }
     }
-  }
-}
+   }
+ })
