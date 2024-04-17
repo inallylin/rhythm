74c9ef0e169bff76f3d269ab88fff68e222f69f5
Ally Lin <inallylin@gmail.com>
2024-04-18 01:04:12 +0800

    try deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,9 +13,10 @@ cd dist
 # echo 'www.example.com' > CNAME
 
 git init
-git add -A
+git add .
 git commit -m 'deploy'
 
+git branch -M gh-pages
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
